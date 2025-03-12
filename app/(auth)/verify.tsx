46c4afa295d69ad5c7a5eac681d86db42ab0bc44
ranselorm <ranselorm@outlook.com>
2025-03-12 23:08:39 +0000

    Remove stylesheet

--- app/(auth)/verify.tsx
+++ app/(auth)/verify.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
 
 const Verify = () => {
@@ -10,5 +10,3 @@ const Verify = () => {
 };
 
 export default Verify;
-
-const styles = StyleSheet.create({});
