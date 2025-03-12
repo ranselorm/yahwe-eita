229b97756ed24092650605ec3f0df6a2a528a9e6
ranselorm <ranselorm@outlook.com>
2025-03-12 10:22:24 +0000

    Modify routing

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Tabayout() {
+  return (
+    <View>
+      <Text>Tab</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,10 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <ThemeProvider>
-      <Stack screenOptions={{ headerShown: false }} />
+      <Stack>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
     </ThemeProvider>
   );
 }
