c800368268288207c90b40c87ed6fd589eb25358
ranselorm <ranselorm@outlook.com>
2025-03-13 14:47:23 +0000

    Style tabbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,7 @@
 import { Tabs, Redirect } from "expo-router";
+import Octicons from "@expo/vector-icons/Octicons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { View } from "react-native";
 
 export default function TabsLayout() {
   // const isAuthenticated = false;
@@ -7,10 +10,60 @@ export default function TabsLayout() {
   // }
 
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="genealogy" options={{ title: "Genealogy" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    <Tabs
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        tabBarInactiveTintColor: "#000",
+        tabBarActiveTintColor: "#000",
+        tabBarStyle: {
+          backgroundColor: "white",
+          borderColor: "#D3D3D3",
+          borderTopWidth: 0.5,
+          height: 60,
+          justifyContent: "center",
+          alignItems: "center",
+          paddingBottom: 10,
+          paddingTop: 10,
+        },
+        tabBarShowLabel: false,
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Octicons name="home" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="genealogy"
+        options={{
+          title: "Genealogy",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="account-group-outline"
+              size={30}
+              color="black"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="account-outline"
+              size={30}
+              color="black"
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/genealogy.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Genealogy = () => {
+  return (
+    <View>
+      <Text>Genealogy</Text>
+    </View>
+  );
+};
+
+export default Genealogy;
