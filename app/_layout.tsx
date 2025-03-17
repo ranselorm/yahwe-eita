5628600595b64a4d462480438fac6101fbcf51d5
ranselorm <ranselorm@outlook.com>
2025-03-17 09:03:43 +0000

    Install axios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,21 @@
 import { ThemeProvider } from "@/context/ThemeProvider";
 import "../global.css";
 import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(auth)" />
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="settings" />
-        <Stack.Screen name="notifications" />
-      </Stack>
-    </ThemeProvider>
+    <QueryClientProvider>
+      <ThemeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="settings" />
+          <Stack.Screen name="notifications" />
+        </Stack>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
