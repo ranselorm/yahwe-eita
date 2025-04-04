e06c691dad841d9f83523496fb61beb4cdd47590
ranselorm <ranselorm@outlook.com>
2025-04-04 08:17:57 +0000

    Fix search params error

--- app/(auth)/confirmation.tsx
+++ app/(auth)/confirmation.tsx
@@ -4,6 +4,8 @@ import { useLocalSearchParams } from "expo-router";
 import React from "react";
 
 export default function ConfirmationScreen() {
+  const { sponsor } = useLocalSearchParams();
+  console.log(sponsor);
   return (
     <View>
       <Text>confirmation</Text>
