fb3d3514daddf1ba983cd86501d1307bc6c6e9c5
ranselorm <ranselorm@outlook.com>
2025-04-04 06:21:08 +0000

    Refactor sponsor hook

--- hooks/useSponsor.ts
+++ hooks/useSponsor.ts
@@ -1,16 +1,24 @@
-import { useMutation } from "@tanstack/react-query";
+// src/hooks/useInvitedUsers.ts
+import { useUser } from "@/context/userContext";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const VERIFY_SPONSOR =
-  "https://yahwe-eita-api-dev.azurewebsites.net/api/sponsor";
+const API_URL = "https://yahwe-eita-api-dev.azurewebsites.net/api/sponsor";
 
-export const useSponsor = () => {
-  return useMutation({
-    mutationFn: async ({ phone }: { phone: string }) => {
-      const response = await axios.post(`${VERIFY_SPONSOR}?pHONE=${phone}`, {
-        headers: { "Content-Type": "application/json" },
-      });
-      return response.data;
-    },
+// Now takes phoneNumber as param
+const getSponsor = async (phone: string) => {
+  const response = await axios.get(API_URL, {
+    // headers: { Authorization: `Bearer ${token}` },
+    params: phone ? { phone: phone } : {},
+  });
+  return response.data?.data ?? [];
+};
+
+// Accepts optional phoneNumber param
+export const useInvitedUsers = (phone: string) => {
+  return useQuery({
+    queryKey: ["getSponsor", phone],
+    queryFn: () => getSponsor(phone),
+    enabled: !!phone,
   });
 };
