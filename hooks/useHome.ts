648b54c20b5239181e31244a4a40cdaf6b231d0d
ranselorm <ranselorm@outlook.com>
2025-04-07 08:50:54 +0000

    Add hook to fetch dashboard

---
+++ hooks/useHome.ts
@@ -0,0 +1,36 @@
+import { useUser } from "@/context/userContext";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const API_URL = "https://yahwe-eita-api-dev.azurewebsites.net/api/home";
+
+const fetchHome = async (token: string) => {
+  try {
+    const response = await axios.get(API_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data?.data;
+  } catch (error) {
+    console.error("Error fetching genealogy:", error);
+    throw error;
+  }
+};
+
+export const useHome = () => {
+  const { user } = useUser();
+  const token = user?.token;
+
+  return useQuery({
+    queryKey: ["home", token],
+    queryFn: async () => {
+      if (token) {
+        const data = await fetchHome(token);
+        return data;
+      }
+      throw new Error("No token or user_id found");
+    },
+    enabled: !!token,
+  });
+};
