7b96c42c5401bada79b371dd383f6b9a62694b93
ranselorm <ranselorm@outlook.com>
2025-04-10 19:25:14 +0000

    Refactor

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,62 @@
-import { ThemeProvider } from "@/context/ThemeProvider";
-import "../global.css";
-import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { UserProvider } from "@/context/userContext";
-import Toast from "react-native-toast-message";
+import { UserProvider, useUser } from "@/context/userContext";
+import { ThemeProvider } from "@/context/ThemeProvider";
 import { CountdownProvider } from "@/context/CountdownContext";
 import { KeyboardProvider } from "react-native-keyboard-controller";
+import { Stack } from "expo-router";
+import Toast from "react-native-toast-message";
+import * as SplashScreen from "expo-splash-screen";
+import { getUserData } from "@/utils";
 
 const client = new QueryClient();
 
-export default function RootLayout() {
+const AppContent = () => {
+  const [loading, setLoading] = useState(true);
+  const { setUser } = useUser();
+  SplashScreen.preventAutoHideAsync();
+
+  useEffect(() => {
+    const initializeApp = async () => {
+      try {
+        const user = await getUserData();
+        if (user) {
+          setUser(user);
+        } else {
+          setUser(null);
+        }
+      } catch (e) {
+        console.warn("Error checking user state:", e);
+      } finally {
+        setLoading(false);
+        SplashScreen.hideAsync();
+      }
+    };
+
+    initializeApp();
+  }, []);
+
+  if (loading) {
+    return null;
+  }
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(auth)" />
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="settings" />
+      <Stack.Screen name="notifications" />
+    </Stack>
+  );
+};
+
+function RootLayoutWithSplash() {
   return (
     <QueryClientProvider client={client}>
       <UserProvider>
         <CountdownProvider>
           <ThemeProvider>
             <KeyboardProvider>
-              <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="(auth)" />
-                <Stack.Screen name="(tabs)" />
-                <Stack.Screen name="settings" />
-                <Stack.Screen name="notifications" />
-              </Stack>
+              <AppContent />
             </KeyboardProvider>
           </ThemeProvider>
         </CountdownProvider>
@@ -30,3 +65,5 @@ export default function RootLayout() {
     </QueryClientProvider>
   );
 }
+
+export default RootLayoutWithSplash;
