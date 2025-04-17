b0f2e55f0440d588dbe9c0fcf6f40689477e6a4f
ranselorm <ranselorm@outlook.com>
2025-04-17 16:38:16 +0000

    Modify hook

--- hooks/useVerifyGhanaCard.tsx
+++ hooks/useVerifyGhanaCard.tsx
@@ -1,42 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "@/store/store";
 import axios from "axios";
-import { setUsers } from "@/store/userSlice";
 
-const API_URL = "https://yahwe-eita-api.azurewebsites.net/api/users";
+const API_URL = "https://yahwe-eita-api.azurewebsites.net/api/verify";
 
-const fetchUsers = async (
-  token: string,
-  page?: number,
-  page_size?: number,
-  search?: string
-) => {
+const fetchGhanaCard = async (type: string, id: string) => {
   const response = await axios.get(API_URL, {
-    headers: { Authorization: `Bearer ${token}` },
     params: {
-      page,
-      page_size,
-      ...(search ? { search } : {}),
+      type,
+      id,
     },
   });
 
-  const users = response?.data?.data?.users ?? [];
-  const count = response?.data?.data?.count ?? 0;
-  return { users, count };
+  return response?.data;
 };
 
-export const useUsers = (page?: number, limit?: number, search?: string) => {
-  const token = useSelector((state: RootState) => state.user.token);
-  const dispatch = useDispatch();
-
-  return useQuery<any, Error>({
-    queryKey: ["users", token, search, page, limit],
+export const useVerifyGhanaCard = (type: string, id: string) => {
+  return useQuery({
+    queryKey: ["ghanaCard", type, id],
     queryFn: async () => {
-      const { users, count } = await fetchUsers(token!, page, limit, search);
-      dispatch(setUsers(users));
-      return { users, count };
+      const data = await fetchGhanaCard(type, id);
+      return data;
     },
-    enabled: !!token,
+    enabled: !!id,
   });
 };
