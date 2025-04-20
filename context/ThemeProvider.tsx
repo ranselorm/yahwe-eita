e1c7f6b553d19a4e9ee1aded3e0f44c0e66af736
ranselorm <ranselorm@outlook.com>
2025-04-20 19:41:48 +0000

    Clean up

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { useColorScheme } from "react-native"; // Use react-native's useColorScheme
+import { useColorScheme } from "react-native";
 
 type Theme = "light" | "dark";
 
@@ -11,20 +11,16 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const systemColorScheme = useColorScheme(); // Use react-native's useColorScheme
+  const systemColorScheme = useColorScheme();
   const [theme, setTheme] = useState<Theme>(systemColorScheme || "light");
 
   useEffect(() => {
-    // If the theme is set manually, use that; otherwise, follow system color scheme
     if (systemColorScheme) {
       setTheme(systemColorScheme);
     }
   }, [systemColorScheme]);
 
-  useEffect(() => {
-    // This effect will update the theme
-    // You can add logic here to reflect the theme on your app, e.g., via nativewind or manual styling
-  }, [theme]);
+  useEffect(() => {}, [theme]);
 
   const toggleTheme = () => {
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
