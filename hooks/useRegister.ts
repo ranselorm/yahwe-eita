67e1b3922da2855c882193f5e67cb68cd2fdf241
ranselorm <ranselorm@outlook.com>
2025-04-24 09:13:15 +0000

    Modify hook

--- hooks/useRegister.ts
+++ hooks/useRegister.ts
@@ -1,18 +1,19 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
+interface UserData {
+  fullName: string;
+  email: string;
+  password: string;
+  phone: string;
+  ghanaCardNumber: string;
+}
+
 const API_URL = "https://yahwe-eita-api.azurewebsites.net/api/register";
 
-// Simple mutation hook for user registration
 export const useRegister = () => {
   return useMutation({
-    mutationFn: async (userData: {
-      fullName: string;
-      email: string;
-      password: string;
-      phone: string;
-      ghanaCardNumber: string;
-    }) => {
+    mutationFn: async (userData: UserData) => {
       const response = await axios.post(API_URL, userData, {
         headers: { "Content-Type": "application/json" },
       });
